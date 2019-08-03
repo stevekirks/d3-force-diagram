85812e5f4caeeb33f4c4a59a211fa3e68b66c58f
stevekirks <SJKirkegard@gmail.com>
2019-08-03 05:51:26 +0000

    v2.2.18

---
+++ precache-manifest.bd49887c978723f943a7fafae7db1b33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e50ac31c301107dc6a5d",
+    "url": "./static/js/main.c72e6d77.chunk.js"
+  },
+  {
+    "revision": "b0d62f460a67b184929d",
+    "url": "./static/js/2.f73c5986.chunk.js"
+  },
+  {
+    "revision": "e50ac31c301107dc6a5d",
+    "url": "./static/css/main.87ca37a4.chunk.css"
+  },
+  {
+    "revision": "5e33532b09a49201acf768c6570cac7a",
+    "url": "./index.html"
+  }
+];
