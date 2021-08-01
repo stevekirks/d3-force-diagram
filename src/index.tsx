9dee9bddd7145c753cf224ed9f3a16cf85778e5a
stevekirks <5696096+stevekirks@users.noreply.github.com>
2021-08-01 13:48:10 +1000

    Working

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,142 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
 import reportWebVitals from './reportWebVitals';
+import './forms.css';
+import * as diagram from './diagram';
+import { getUrlParameterAsArray, setUrlParameterAsArray, getUrlParameter, setUrlParameter } from './utils/url-query-param-utils';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const clickme = () => {
+    console.log('blablabla');
+}
+(document.getElementById('btnHighlight') as HTMLButtonElement).onclick = clickme;
+
+const urlParamLabelShowOnlyHighlighted = 'showHlOnly';
+const urlParamLabelHighlightedNodes = 'hlNodes';
+
+let initShowOnlyHighlighted = getUrlParameter(urlParamLabelShowOnlyHighlighted) === 'y';
+const initHighlightedNodeNames = getUrlParameterAsArray(urlParamLabelHighlightedNodes);
+if (initShowOnlyHighlighted && initHighlightedNodeNames.length === 0) {
+    setUrlParameter(urlParamLabelShowOnlyHighlighted, '');
+    initShowOnlyHighlighted = false;
+}
+
+let inputHighlightText = '';
+let showAllLabels = false;
+let showOnlyHighlighted = initShowOnlyHighlighted;
+let highlightedNodeNames = initHighlightedNodeNames;
+let invertBackground = false;
+let hasForceSimulation = true;
+
+const setInputHighlightText = (pInputHighlightText: string) => {
+    inputHighlightText = pInputHighlightText;
+    (document.getElementById('inInputSearch') as HTMLInputElement).value = pInputHighlightText;
+};
+
+const setShowAllLabels = (pShowAllLabels: boolean) => {
+    showAllLabels = pShowAllLabels;
+    (document.getElementById('chkboxShowAllLabels') as HTMLInputElement).checked = pShowAllLabels;
+};
+
+const setShowOnlyHighlighted = (pShowOnlyHighlighted: boolean) => {
+    showOnlyHighlighted = pShowOnlyHighlighted;
+    (document.getElementById('lblSearch') as HTMLLabelElement).className = pShowOnlyHighlighted ? 'disabled' : '';
+    (document.getElementById('inInputSearch') as HTMLInputElement).disabled = pShowOnlyHighlighted;
+    (document.getElementById('btnHighlight') as HTMLButtonElement).disabled = pShowOnlyHighlighted;
+    (document.getElementById('chkboxShowAllLabels') as HTMLInputElement).disabled = pShowOnlyHighlighted;
+    (document.getElementById('chkboxShowOnlyHighlighted') as HTMLInputElement).checked = pShowOnlyHighlighted;
+}
+
+const setHighlightedNodeNames = (pHighlightedNodeNames: string[]) => {
+    highlightedNodeNames = pHighlightedNodeNames;
+    (document.getElementById('chkboxShowOnlyHighlighted') as HTMLButtonElement).disabled = pHighlightedNodeNames.length === 0;
+}
+
+const setInvertBackground = (pInvertBackground: boolean) => {
+    invertBackground = pInvertBackground;
+    (document.getElementById('chkboxInvertBackground') as HTMLInputElement).checked = pInvertBackground;
+    (document.getElementById('diagram') as HTMLDivElement).className = pInvertBackground ? 'inverted' : '';
+}
+
+const setHasForceSimulation = (pHasForceSimulation: boolean) => {
+    hasForceSimulation = pHasForceSimulation;
+    (document.getElementById('chkboxHasForceSimulation') as HTMLInputElement).checked = pHasForceSimulation;
+}
+
+const updateShowAllLabels = (pShowAllLabels: boolean) => {
+    setShowAllLabels(pShowAllLabels);
+    diagram.showAllLabels(pShowAllLabels);
+}
+
+const updateInputHighlightText = (txt: string) => {
+    setInputHighlightText(txt);
+    diagram.searchForNodes(txt);
+}
+
+const handleHighlightedNodesChanged = (pHighlightedNodeNames: string[]) => {
+    setHighlightedNodeNames(pHighlightedNodeNames);
+    setUrlParameterAsArray(urlParamLabelHighlightedNodes, pHighlightedNodeNames);
+    if (pHighlightedNodeNames.length === 0 && showOnlyHighlighted) {
+        setShowOnlyHighlighted(false);
+        setUrlParameter(urlParamLabelShowOnlyHighlighted, '');
+    }
+}
+
+const handleSearchForNodesClick = () => {
+    diagram.searchForNodes(inputHighlightText);
+}
+
+const handleInputHighlightText = (event: Event): void => {
+    const newVal = (event.currentTarget as HTMLInputElement).value;
+    updateInputHighlightText(newVal);
+    event.preventDefault();
+}
+
+const handleShowAllLabels = (event: Event) => {
+    const newVal = (event.currentTarget as HTMLInputElement).checked;
+    updateShowAllLabels(newVal);
+    console.log('handleShowAllLabels: ' + newVal);
+}
+
+const handleShowOnlyHighlighted = (event: Event) => {
+    const newVal = (event.currentTarget as HTMLInputElement).checked;
+    setShowOnlyHighlighted(newVal);
+    setUrlParameter(urlParamLabelShowOnlyHighlighted, newVal ? 'y' : '');
+    diagram.showOnlyHighlighted(newVal);
+    if (newVal === true && showAllLabels === true) {
+        updateShowAllLabels(false);
+    }
+}
+
+const handleInvertBackground = (event: Event) => {
+    const newVal = (event.currentTarget as HTMLInputElement).checked;
+    setInvertBackground(newVal);
+    diagram.invertBackground(newVal);
+}
+
+const handleHasForceSimulation = (event: Event) => {
+    const newVal = (event.currentTarget as HTMLInputElement).checked;
+    setHasForceSimulation(newVal);
+    diagram.setHasForceSimulation(newVal);
+}
+
+const initialiseApp = () => {
+    setInputHighlightText(inputHighlightText);
+    setShowAllLabels(showAllLabels);
+    setShowOnlyHighlighted(showOnlyHighlighted);
+    setHighlightedNodeNames(highlightedNodeNames);
+    setInvertBackground(invertBackground);
+    setHasForceSimulation(hasForceSimulation);
+
+    (document.getElementById('btnHighlight') as HTMLButtonElement).onclick = handleSearchForNodesClick;
+    (document.getElementById('inInputSearch') as HTMLInputElement).onchange = handleInputHighlightText;
+    (document.getElementById('chkboxShowAllLabels') as HTMLInputElement).onchange = handleShowAllLabels;
+    (document.getElementById('chkboxShowOnlyHighlighted') as HTMLInputElement).onchange = handleShowOnlyHighlighted;
+    //(document.getElementById('chkboxInvertBackground') as HTMLInputElement).onchange = handleInvertBackground;
+    (document.getElementById('chkboxInvertBackground') as HTMLInputElement).onclick = () => console.log('bla');
+    (document.getElementById('chkboxHasForceSimulation') as HTMLInputElement).onchange = handleHasForceSimulation;
+    diagram.load(handleHighlightedNodesChanged, highlightedNodeNames, showOnlyHighlighted);
+}
+
+window.onload = initialiseApp;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
